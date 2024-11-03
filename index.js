904392ef10c2f6485e4bb1dd72ba6808a44287a7
cadetvivek <kushwahvivek805@gmail.com>
2024-11-03 10:50:08 +0530

    HOF

---
+++ index.js
@@ -0,0 +1,24 @@
+// // Interactive Coding Assistant: Aplying Higher-Order Functions(HOF)
+
+// //Map
+// let arr = [1,2,3,4,5]
+// let Squre = arr.map((ele)=>{
+//     return ele**2
+// })
+// console.log(Squre)
+
+
+// //Filter
+// let arr = [1,2,3,4,5]
+// let EvenArr = arr.filter((ele)=>{
+//     return ele%2==0
+// })
+// console.log(EvenArr)
+
+
+// //Reduce
+// let arr = [5,10,15];
+// let Sum = arr.reduce((ele,index)=>{
+//     return ele + index
+// },0)
+// console.log(Sum)
